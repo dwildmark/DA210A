a0dd62e87d7b47e057c2d983ce33f1c086e04abf
Jonathan <jonathan.e.m.bocker@gmail.com>
2016-05-13 17:17:32 +0200

    Features and error handling added
    Serial Com error handling implemented.
    Graceful shutdown with touchscreen implemented.
    Increased pattern matching implemented, now pattern is triangle within
    rectangle.
    Controller algorithm now sends desired speed instead of pulse width
    length

--- Pycharm/testRoboto.py
+++ Pycharm/testRoboto.py
@@ -8,16 +8,17 @@ import imutils.video.videostream as imv
 from sys import platform as _platform
 import socket
 
+running = True
 screenwidth = 320
 screenheight = 240
 camera = cv2.VideoCapture(0)
 screenrads = 53.5 * math.pi / 180
-radiansPerPixel = screenrads / screenwidth
+radiansPerPixel = screenrads / float(screenwidth)
 maxDist = 400 #cm
 targetWidth = 20 #cm
 targetHeight = 20 #cm
-minWidth = (targetWidth/maxDist) * radiansPerPixel
-minHeight = (targetHeight/maxDist) * radiansPerPixel
+minWidth = (targetWidth/float(maxDist)) / radiansPerPixel
+minHeight = (targetHeight/float(maxDist)) / radiansPerPixel
 
 def getIP():
     try:
@@ -25,14 +26,30 @@ def getIP():
     except:
         return "Not connected to Wifi"
 
+def click_and_close(event, x, y, flags, param):
+    global running
+    running = False 
 
-#ser = serial.Serial(
-#    port='/dev/ttyACM2', baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,
-#    timeout=0, writeTimeout=0)
+def find_triangle(image):
+    (_, cnts, _) = cv2.findContours(image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for c in cnts:
+    # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.05 * peri, True)
 
-#print("connected to: " + ser.portstr)
+        if 3 == len(approx):
+            # compute the solidity of the original contour
+            area = cv2.contourArea(c)
+            hullArea = cv2.contourArea(cv2.convexHull(c))
+            solidity = area / float(hullArea)
+            if solidity > 0.8:
+                return True
+
+    return False
+
+
 if _platform == "linux2":
-    video = imv.VideoStream(usePiCamera=True, resolution=(screenwidth,screenheight), framerate=15)
+    video = imv.VideoStream(usePiCamera=True, resolution=(screenwidth,screenheight), framerate=30)
 else:
     video = imv.VideoStream(resolution=(640,480))
 video.start()
@@ -40,6 +57,7 @@ video.start()
 #Fullscreen
 cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.setMouseCallback("Frame", click_and_close)
 
 ports = list(serial.tools.list_ports.comports())
 addr = ''
@@ -47,18 +65,21 @@ for p in ports:
     print p[0]
     if '/dev/ttyACM' in p[0]:
         addr = str(p[0])
-ser = serial.Serial(
-    port=addr, baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,
-    timeout=0, writeTimeout=0)
-
+try:
+    ser = serial.Serial(
+        port=addr, baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,
+        timeout=0, writeTimeout=0)
+except:
+    ser = None
+    
 # keep looping
 time.sleep(1)
-while True:
+while running:
     #time.sleep(0.1)
     # grab the current frame and initialize the status text
     frame = video.read()
     status = "No Targets"
-    status2 = "1500:1500"
+    status2 = "0:0"
 
     # convert the frame to grayscale, blur it, and detect edges
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -78,7 +99,7 @@ while True:
     for c in cnts:
         # approximate the contour
         peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.10 * peri, True)
+        approx = cv2.approxPolyDP(c, 0.05 * peri, True)
 
         # ensure that the approximated contour is "roughly" rectangular
         if 4 <= len(approx) <= 5:
@@ -94,20 +115,21 @@ while True:
 
             # compute whether or not the width and height, solidity, and
             # aspect ratio of the contour falls within appropriate bounds
-            keepDims = w > 20 and h > 20
+            keepDims = w > minWidth and h > minHeight
             keepSolidity = solidity > 0.9
-            keepAspectRatio = 0.8 <= aspectRatio <= 1.2
+            keepAspectRatio = 0.7 <= aspectRatio <= 1.3
+            larger = area > largestArea
+            triangle_present = find_triangle(edged.copy()[y:y+h, x:x+w])
 
             # ensure that the contour passes all our tests
-            if keepDims and keepSolidity and keepAspectRatio:
-                if area > largestArea and w > minWidth and h > minHeight:
-                    largestArea = area
-                    distance = 24.5 / (radiansPerPixel * max(h, w))
-                    largestApprox = approx
+            if keepDims and keepSolidity and keepAspectRatio and larger and triangle_present:
+                largestArea = area
+                distance = 24.5 / (radiansPerPixel * max(h, w))
+                largestApprox = approx
                 
     if largestArea > 0:
         # draw an outline around the target and update the status text
-        cv2.drawContours(frame, [largestApprox], -1, (0, 0, 255), 4)
+        cv2.drawContours(frame, [largestApprox], -1, (0, 0, 125), 2)
         status = "D: " + str(int(distance)) + "cm "
         # compute the center of the contour region and draw the
         # crosshairs
@@ -115,42 +137,64 @@ while True:
         (cX, cY) = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
         (startX, endX) = (int(cX - (w * 0.15)), int(cX + (w * 0.15)))
         (startY, endY) = (int(cY - (h * 0.15)), int(cY + (h * 0.15)))
-        cv2.line(frame, (startX, cY), (endX, cY), (0, 0, 255), 3)
-        cv2.line(frame, (cX, startY), (cX, endY), (0, 0, 255), 3)
+        cv2.line(frame, (startX, cY), (endX, cY), (0, 0, 125), 2)
+        cv2.line(frame, (cX, startY), (cX, endY), (0, 0, 125), 2)
 
         # Calculate pulsewidths for Arduino
         middleX = frame.shape[1] / 2
 
-        sideError = (middleX - cX) * radiansPerPixel
+        sideError = (middleX - cX) * float(radiansPerPixel)
         distanceError = (distance - 100) * 4
         distanceError = min(max(-500, distanceError), 500)
+        v = max(math.exp((distance-100)/5), 70)
+        degrees = (sideError)*180/math.pi
+ 
+        deltaV = math.sqrt(math.fabs(degrees*20))
+ 
+        if degrees > 0:
+           VL = int(max((v + deltaV), 80))
+           VR = int(max((v - deltaV), 80))
+        else:
+           VL = int(max((v - deltaV), 80))
+           VR = int(max((v + deltaV), 80))
+ 
+        
         PWL = (distanceError + 1500) + (sideError / 2) * 1000
         PWR = (distanceError + 1500) - (sideError / 2) * 1000
 
         PWL = int(min(max(1000, PWL), 2000))
         PWR = int(min(max(1000, PWR), 2000))
 
-        stringen = str(PWL) + ":" + str(PWR)
-        status2 = stringen
-        ser.write(stringen + "\r\n")
+        VL = int((PWL - 1500) / 6.25)
+        VR = int((PWR - 1500) / 6.25)
+        
+        if ser is not None:
+            stringen = str(VL) + ":" + str(VR) 
+            status2 = stringen
+            ser.write(stringen + "\r\n")
         #print(ser.readline())
 
     else:
-        ser.write("1500:1500" + "\r\n")
-    status2 += " " + ser.readline()
-        
+        if ser is not None:
+            ser.write("0:0" + "\r\n")
 
-
+    if ser is not None:
+        status2 += " " + ser.readline()
 
     cv2.putText(frame, getIP(), (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 150), 2)
     cv2.putText(frame, status, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 150), 2)
     cv2.putText(frame, status2, (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 150), 2)
     cv2.imshow("Frame", frame)
+
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
 
+    if not running:
+        break
+
 # cleanup the camera and close any open windows
-ser.close()
+if ser is not None:
+    ser.close()
 camera.release()
 cv2.destroyAllWindows()
